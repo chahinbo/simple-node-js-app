8c237b0f27a9198f3801c551a356a882d6a3d8cd
Chahin BEN OTHMEN <PROXYM-IT\chahin.ben-othmen@prx-lp00399.proxym-it.tn>
2021-01-27 22:36:23 +0100

    FEATURE : custom output

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'SECCUSS' });
+    return res.json({ state: 'SECCUSS', message: 'ACHRAF YATIK B ASBA' });
 });
 
 app.listen(8080, () => {
